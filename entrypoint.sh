0afd464a63d08b178e2382df8c0e21ebd0b058f5
estebanmcalbandi <estebanmc@outlook.es>
2025-12-28 11:06:43 +0100

    VAmos

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,35 +1,55 @@
 #!/bin/sh
 
 # Si no existe el fichero de configuración, es la primera vez que se lanza
+# Creamos una configuración personalizada
 if [ ! -f /var/lib/mldonkey/downloads.ini ]; then
-    mldonkey &
+    su - mldonkey -c "MLDONKEY_DIR=\"$MLDONKEY_DIR\" mldonkey" &
 
     echo 'Waiting for mldonkey to start...'
     sleep 3
 
-    /usr/lib/mldonkey/mldonkey_command -p "" "set client_name Iiiiiiiiiiiiiiiiiiiiii'm" "save"
-    /usr/lib/mldonkey/mldonkey_command -p "" "set client_buffer_size 5000000" "save"
+     # Aunque lo ejecute root, la ejecición la realiza el usuario mldonkey
     /usr/lib/mldonkey/mldonkey_command -p "" "set run_as_user mldonkey" "save"
+    
+    # Nombre del cliente
+    /usr/lib/mldonkey/mldonkey_command -p "" "set client_name emc2" "save"
+
+    # Client buffer size a 5MB
+    /usr/lib/mldonkey/mldonkey_command -p "" "set client_buffer_size 5000000" "save"
+
+   
+    # IPs desde las que se permite la conexión
     /usr/lib/mldonkey/mldonkey_command -p "" "set allowed_ips 0.0.0.0/0" "save"
+
+    # Activo kad
     /usr/lib/mldonkey/mldonkey_command -p "" "set enable_kademlia true" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set enable_bittorrent false" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set enable_overnet false" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set enable_directconnect false" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set enable_fileTP false" "save"
+
+    # Diversos valores de configuración
     /usr/lib/mldonkey/mldonkey_command -p "" "set max_hard_upload_rate 1000" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set max_hard_download_rate 0" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set max_upload_slots 10" "save"
+
+    # Configuro ED2K
     /usr/lib/mldonkey/mldonkey_command -p "" "set ED2K-connect_only_preferred_server false" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set ED2K-max_connected_servers 4" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set ED2K-min_left_servers 5" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set ED2K-firewalled-mode false" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set ED2K-port 20562" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set ED2K-upload_timeout 60." "save"
+
     /usr/lib/mldonkey/mldonkey_command -p "" "set max_concurrent_downloads 150" "save"
+    
+    # Descargas y sus permisos
     /usr/lib/mldonkey/mldonkey_command -p "" "set filenames_utf8 true" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set create_file_mode 644" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set create_dir_mode 755" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set create_file_sparse true" "save"
+
+    # Algunas listas que hay que actualizar
     /usr/lib/mldonkey/mldonkey_command -p "" "urlremove http://www.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "urlremove http://www.gruk.org/server.met.gz" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "urlremove http://update.kceasy.com/update/fasttrack/nodes.gzip" "save"
@@ -39,22 +59,17 @@ if [ ! -f /var/lib/mldonkey/downloads.ini ]; then
     /usr/lib/mldonkey/mldonkey_command -p "" "urladd guarding.p2p http://upd.emule-security.org/ipfilter.zip 250" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "urladd geoip.dat http://upd.emule-security.org/ip-to-country.csv.zip 0" "save"
 
-    # Cambiamos la contraseña si la hay y apagamos el servicio
-    if [ -z "$MLDONKEY_ADMIN_PASSWORD" ]; then
-        /usr/lib/mldonkey/mldonkey_command -p "" "kill"
-    else
-        /usr/lib/mldonkey/mldonkey_command -p "" "passwd ${MLDONKEY_ADMIN_PASSWORD}"
-        /usr/lib/mldonkey/mldonkey_command -p "${MLDONKEY_ADMIN_PASSWORD}" "kill"
-    fi
+    # Cambiamos la contraseña si la hay y apagamos el servicio    
+    /usr/lib/mldonkey/mldonkey_command -p "" "passwd 'P@ssw0rd'"
+    /usr/lib/mldonkey/mldonkey_command -p 'P@ssw0rd' "kill"
+    
 
     # First port 6209 is for overnet, second 16965 for kad, we leave all the same
+    # Ojo: Overnet ya no funciona
     sed -i '0,/   port =/s/   port =.*/  port = 6209/' /var/lib/mldonkey/donkey.ini
     sed -i '0,/   port =/s/   port =.*/  port = 16965/' /var/lib/mldonkey/donkey.ini
     sed -i 's/  port =/   port =/' /var/lib/mldonkey/donkey.ini
 fi
 
-# Reinicio los permisos en /var/lib/
-chown -R mldonkey:mldonkey /var/lib/mldonkey
-
 # Lanzo finalmente el servicio
-mldonkey
+su - mldonkey -c "MLDONKEY_DIR=\"$MLDONKEY_DIR\" mldonkey"
